279fb3c1784290aad49f76fa5b137b7cdb3f96ef
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-06-24 00:10:00 +0300

    task-02 +

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,30 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+// ВАРІАНТ 1
+ingredients.map((ingredient) => {
+  const itemRef = document.createElement("li");
+  itemRef.textContent = ingredient;
+  itemRef.classList.add("item");
+  ingredientsRef.append(itemRef);
+
+  console.log("itemRef:", itemRef);
+});
+
+// ВАРІАНТ 2
+/* ingredients.forEach((item) => {
+  const itemRef = document.createElement("li");
+  itemRef.textContent = item;
+  itemRef.classList.add("item");
+  ingredientsRef.append(itemRef);
+
+  console.log("itemRef:", itemRef);
+}); */
