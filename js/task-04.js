b346eff27e484e25ea03a91a38ed48753ecf703a
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-06-24 00:28:25 +0300

    task-04 +

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+const refs = {
+  value: document.querySelector("#value"),
+  decrement: document.querySelector('#counter button[data-action="decrement"]'),
+  increment: document.querySelector('#counter button[data-action="increment"]'),
+  // АБО
+  // button: document.querySelectorAll("#counter button"),
+};
+
+refs.decrement.addEventListener("click", onClickAddDecrement);
+refs.increment.addEventListener("click", onClickAddIncrement);
+// АБО
+// refs.button[0].addEventListener("click", onClickAddDecrement);
+// refs.button[1].addEventListener("click", onClickAddIncrement);
+
+let counterValue = 0;
+
+function onClickAddDecrement(event) {
+  counterValue -= 1;
+  refs.value.textContent = counterValue;
+}
+function onClickAddIncrement(event) {
+  counterValue += 1;
+  refs.value.textContent = counterValue;
+}
