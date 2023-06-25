b08c39e97fa9abab85adca86570bc357e0205ecd
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-06-25 17:16:39 +0300

    task-09 +

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const refs = {
+  colorRef: document.querySelector(".color"),
+  buttonRef: document.querySelector(".change-color"),
+};
+
+refs.buttonRef.addEventListener("click", onClickChangeColor);
+
+function onClickChangeColor() {
+  refs.colorRef.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
