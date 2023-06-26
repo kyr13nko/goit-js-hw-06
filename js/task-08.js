ea3c06dc9d3139d13712d751df53ad3a0f396c75
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-06-26 14:50:44 +0300

    task-08 +

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    return alert("У формі є незаповнені поля");
+  }
+
+  const obj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(obj);
+
+  event.currentTarget.reset();
+}
