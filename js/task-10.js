52ec1e8e8f4570d483a272bf8e82dc74512c8439
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-06-27 11:22:39 +0300

    task-10 +

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
+const refs = {
+  boxes: document.querySelector("#boxes"),
+  input: document.querySelector("#controls input"),
+
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+};
+
+refs.btnCreate.addEventListener("click", createBoxes);
+refs.btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = refs.input.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    const divRef = document.createElement("div");
+
+    divRef.style.backgroundColor = getRandomHexColor();
+    divRef.style.height = `${30 + i * 10}px`;
+    divRef.style.width = `${30 + i * 10}px`;
+    divRef.style.margin = "10px";
+
+    refs.boxes.style.display = "flex";
+    refs.boxes.style.flexWrap = "wrap";
+
+    refs.boxes.append(divRef);
+  }
+}
+
+function destroyBoxes() {
+  return (refs.boxes.innerHTML = "");
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
