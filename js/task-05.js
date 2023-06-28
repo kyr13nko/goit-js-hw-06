11fb93655f93b9fc5cc39b989b59194044549c8b
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-06-28 13:49:39 +0300

    task-05++

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,18 @@ const textRef = document.querySelector("#name-output");
 inputRef.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
+  textRef.textContent =
+    event.currentTarget.value.trim() !== ""
+      ? event.currentTarget.value.trim()
+      : "Anonymous";
+}
+
+/* function onInputChange(event) {
   textRef.textContent = event.currentTarget.value;
 
   textRef.textContent =
-    textRef.textContent === "" ? "Anonymous" : textRef.textContent;
+    textRef.textContent.trim() === "".trim()
+      ? "Anonymous"
+      : textRef.textContent.trim();
 }
+ */
